587656f7ddae6cfb9212bbff3689a6648922f9f9
zaenalrfn <ZAOfficial24@gmail.com>
2025-07-21 22:11:42 +0700

    feat: reverb

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,10 +6,27 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
-import { configureEcho } from '@laravel/echo-vue';
+import Echo from 'laravel-echo';
 
-configureEcho({
+import Pusher from 'pusher-js';
+
+declare global {
+  interface Window {
+    Pusher: any;
+    Echo: any;
+  }
+}
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
     broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
 });
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
